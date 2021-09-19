d2f20f4d669da2ecf882ad9c775247bf1b28b4c6
mwahab2000 <mohamed.abdelwahab2012@gmail.com>
2021-09-19 14:15:18 +0200

    Update employee.controller.spec.ts

--- backend/src/modules/domain/employees/employee.controller.spec.ts
+++ backend/src/modules/domain/employees/employee.controller.spec.ts
@@ -37,7 +37,7 @@ describe('Employee Controller', () => {
         displayName: 'Jimmy',
         effectiveDate: '2018-22-10',
         companyEmail: 'jimmyramos@acklenavenue.com',
-        firstName: 'Jimmy',
+        xfirstName: 'Jimmy',
         gender: 'Male',
         lastName: 'Ramos',
         middleName: 'Josue',
@@ -64,7 +64,7 @@ describe('Employee Controller', () => {
       // Arrange
       const request = {
         employeeId: 100,
-        xfirstName: 'Jimmy',
+        firstName: 'Jimmy',
         lastName: 'Ramos',
         middleName: 'Josue',
         secondLastName: 'Banegas',
